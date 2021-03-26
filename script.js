292977c38de9273f36b23070fc8495fa7ffd30f5
somaratne <erangasomaratne@gmail.com>
2021-03-26 18:17:28 +0530

    added event listeners to buttons

---
+++ script.js
@@ -0,0 +1,40 @@
+const quoteContainer = document.getElementById('quote-container');
+const textQuote = document.getElementById('quote');
+const author = document.getElementById('author');
+const twitterButton = document.getElementById('twitter');
+const newQuoteButton = document.getElementById('new-quote');
+
+let apiQuote = [];
+
+function newQuote(apiQuote){
+    const value = Math.floor(Math.random() * apiQuote.length);
+    textQuote.textContent = apiQuote[value].text;
+    author.textContent = !apiQuote[value].author ? 'Unknown' : apiQuote[value].author ;
+    if(textQuote.textContent.length > 50){
+        textQuote.classList.add('long-quote');
+    }else {
+        textQuote.classList.remove('long-quote');
+    }
+}
+
+async function getQuotes(){
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuote = await response.json();
+        newQuote(apiQuote);
+        
+    } catch (err) {
+        console.log(err.message);
+    }
+}
+
+getQuotes();
+
+function tweetQuote(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${textQuote.textContent} - ${author.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+twitterButton.addEventListener('click',tweetQuote);
+newQuoteButton.addEventListener('click', getQuotes);
